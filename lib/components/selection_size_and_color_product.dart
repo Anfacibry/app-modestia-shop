3390b69da56d3fdb092f9089f7ef480eebab6991
Anfacibry <jogos.fabriciocintra@gmail.com>
2023-12-08 14:58:53 -0300

    :lipstick: feat: Criando componentes para as alterações das cores e tamanhos do produto. Seleção de cores é da linha 10 à linha 73 e seleção de tamanho é da linha 75 a linha 158

---
+++ lib/components/selection_size_and_color_product.dart
@@ -0,0 +1,158 @@
+import 'package:app_fashion_shop/store/data/storage_product.dart';
+import 'package:app_fashion_shop/store/models/store_product.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
+
+import '../../config/style/app_style.dart';
+import '../../config/theme/app_color.dart';
+
+class SelectionColorProduct extends StatelessWidget {
+  final Product product;
+  final double fontSize;
+  final double sizeCircular;
+  final double widthBorder;
+
+  const SelectionColorProduct({
+    required this.fontSize,
+    required this.product,
+    required this.sizeCircular,
+    required this.widthBorder,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Observer(
+            builder: (ctx) => Text(
+              "Cor: ${product.imageColor[0].name}",
+              style: AppStyle.textBody(
+                size: fontSize,
+              ),
+            ),
+          ),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Observer(
+            builder: (ctx) => Row(
+              children: product.imageColor
+                  .map((cores) => Container(
+                        margin: const EdgeInsets.only(right: 5),
+                        height: sizeCircular,
+                        width: sizeCircular,
+                        decoration: BoxDecoration(
+                          color: cores.color,
+                          borderRadius: BorderRadius.circular(35),
+                          border: product.imageColor[0].color == cores.color
+                              ? Border.all(
+                                  color: AppColor.secundaryColor,
+                                  width: widthBorder,
+                                )
+                              : null,
+                          boxShadow: [
+                            AppStyle.shadow(
+                                backgroundColor: Colors.black38,
+                                offset: (dx: 0, dy: 2),
+                                blurRadius: 2)
+                          ],
+                        ),
+                      ))
+                  .toList(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SelectionSizeProduct extends StatelessWidget {
+  final double heightButton;
+  final double widthButton;
+  final double fontSize;
+  final double radiusSize;
+  final double sizePadding;
+  final Product product;
+
+  const SelectionSizeProduct({
+    required this.heightButton,
+    required this.widthButton,
+    required this.radiusSize,
+    required this.fontSize,
+    required this.sizePadding,
+    required this.product,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    StorageProduct storageProduct = Provider.of(context, listen: false);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Tamanho",
+          style: AppStyle.textBody(size: fontSize),
+        ),
+        SingleChildScrollView(
+          clipBehavior: Clip.none,
+          scrollDirection: Axis.horizontal,
+          child: Observer(
+            builder: (ctx) => Row(
+              children: product.size
+                  .map(
+                    (size) => Padding(
+                      padding: EdgeInsets.only(right: sizePadding),
+                      child: InkWell(
+                        onTap: () {
+                          storageProduct.tackSizeProduct(
+                              size: size, product: product);
+                        },
+                        child: Observer(
+                          builder: (ctx) => Container(
+                            height: heightButton,
+                            width: widthButton,
+                            decoration: BoxDecoration(
+                                color: product.tackSize == size
+                                    ? AppColor.primaryColor
+                                    : AppColor.onPrimaryColor,
+                                borderRadius: BorderRadius.all(
+                                    Radius.circular(radiusSize)),
+                                boxShadow: [
+                                  AppStyle.shadow(
+                                    backgroundColor: Colors.black12,
+                                    offset: (dx: 0, dy: 2),
+                                    blurRadius: 2,
+                                  ),
+                                ]),
+                            child: Center(
+                              child: Text(
+                                size,
+                                style: TextStyle(
+                                  color: product.tackSize == size
+                                      ? AppColor.onPrimaryColor
+                                      : AppColor.primaryColor,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: fontSize,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
