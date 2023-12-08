ada064eb816522d66855fa05b064e4c398a6d85d
Anfacibry <jogos.fabriciocintra@gmail.com>
2023-12-08 14:18:09 -0300

    :bug: fix: filtrando os produtos da lista de favoritos

--- lib/screens/favorite.dart
+++ lib/screens/favorite.dart
@@ -1,4 +1,6 @@
 import 'package:app_fashion_shop/components/detail_product/selection_size_and_color_product.dart';
+import 'package:app_fashion_shop/config/routes/named_routes.dart';
+
 import 'package:app_fashion_shop/config/style/app_style.dart';
 import 'package:app_fashion_shop/config/theme/app_color.dart';
 import 'package:app_fashion_shop/store/data/storage_product.dart';
@@ -79,18 +81,27 @@ class FavoriteProduct extends StatelessWidget {
                       padding: EdgeInsets.all(constraints.maxWidth * .02),
                       child: Row(
                         children: [
-                          Container(
-                            height: constraints.maxHeight * .9,
-                            width: constraints.maxWidth * .5,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                image: DecorationImage(
-                                  image: AssetImage(
-                                    storageProduct.listProductFavorite[index]
-                                        .imageColor[0].image,
-                                  ),
-                                  fit: BoxFit.cover,
-                                )),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(
+                                  context, NamedRoutes.routeProductDetail);
+                              storageProduct.getProduct(
+                                  product: storageProduct
+                                      .listProductFavorite[index]);
+                            },
+                            child: Container(
+                              height: constraints.maxHeight * .9,
+                              width: constraints.maxWidth * .5,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                      storageProduct.listProductFavorite[index]
+                                          .imageColor[0].image,
+                                    ),
+                                    fit: BoxFit.cover,
+                                  )),
+                            ),
                           ),
                           Padding(
                             padding: EdgeInsets.only(
@@ -118,46 +129,88 @@ class FavoriteProduct extends StatelessWidget {
                                     storageProduct
                                         .listProductFavorite[index].price
                                         .toString(),
-                                    style: AppStyle.textTitleSecondary(),
+                                    style: AppStyle.textTitleSecondary(
+                                        size: width * .04),
                                   ),
-                                  SizedBox(
-                                    height:
+                                  SelectionColorProduct(
+                                    fontSize:
                                         constraints.maxWidth * .4 > width * .3
-                                            ? constraints.maxHeight * .25
-                                            : constraints.maxHeight * .25,
-                                    width: constraints.maxWidth * .4,
-                                    child: SelectionColorProduct(
-                                      fontSize:
-                                          constraints.maxWidth * .4 > width * .3
-                                              ? constraints.maxWidth * .035
-                                              : constraints.maxWidth * .07,
-                                      product: storageProduct,
-                                      sizeCircular:
-                                          constraints.maxWidth * .4 > width * .3
-                                              ? constraints.maxWidth * .05
-                                              : constraints.maxWidth * .1,
-                                      widthBorder: 02,
+                                            ? constraints.maxWidth * .035
+                                            : constraints.maxWidth * .07,
+                                    sizeCircular:
+                                        constraints.maxWidth * .4 > width * .3
+                                            ? constraints.maxWidth * .05
+                                            : constraints.maxWidth * .1,
+                                    widthBorder: 02,
+                                    product: storageProduct
+                                        .listProductFavorite[index],
+                                  ),
+                                  SelectionSizeProduct(
+                                    heightButton:
+                                        constraints.maxWidth * .4 > width * .3
+                                            ? constraints.maxWidth * .04
+                                            : constraints.maxWidth * .08,
+                                    widthButton:
+                                        constraints.maxWidth * .4 > width * .3
+                                            ? constraints.maxWidth * .04
+                                            : constraints.maxWidth * .08,
+                                    fontSize:
+                                        constraints.maxWidth * .4 > width * .3
+                                            ? constraints.maxWidth * .035
+                                            : constraints.maxWidth * .06,
+                                    radiusSize: constraints.maxWidth * .01,
+                                    sizePadding: constraints.maxWidth * .01,
+                                    product: storageProduct
+                                        .listProductFavorite[index],
+                                  ),
+                                  Padding(
+                                      padding: EdgeInsets.only(
+                                          top: constraints.maxHeight * .04)),
+                                  SizedBox(
+                                    height: width * .04,
+                                    width: width * .2,
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        storageProduct.addProductCart(
+                                            storageProduct
+                                                .listProductFavorite[index]);
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: AppColor.primaryColor,
+                                        foregroundColor:
+                                            AppColor.onPrimaryColor,
+                                        padding: const EdgeInsets.all(0),
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(
+                                                width * .01)),
+                                      ),
+                                      child: Text(
+                                        "Add Carrinho",
+                                        style: TextStyle(
+                                          fontSize: width * .025,
+                                        ),
+                                      ),
                                     ),
                                   ),
-                                  SizedBox(
-                                    height: constraints.maxHeight * .2,
-                                    width: constraints.maxWidth * .4,
-                                    child: SelectionSizeProduct(
-                                      heightButton:
-                                          constraints.maxWidth * .4 > width * .3
-                                              ? constraints.maxWidth * .04
-                                              : constraints.maxWidth * .08,
-                                      widthButton:
-                                          constraints.maxWidth * .4 > width * .3
-                                              ? constraints.maxWidth * .04
-                                              : constraints.maxWidth * .08,
-                                      fontSize:
-                                          constraints.maxWidth * .4 > width * .3
-                                              ? constraints.maxWidth * .035
-                                              : constraints.maxWidth * .07,
-                                      radiusSize: constraints.maxWidth * .01,
-                                      sizePadding: constraints.maxWidth * .01,
-                                      product: storageProduct,
+                                  Padding(
+                                      padding: EdgeInsets.only(
+                                          top: constraints.maxHeight * .03)),
+                                  InkWell(
+                                    onTap: () {
+                                      storageProduct.getProduct(
+                                          product: storageProduct
+                                              .listProductFavorite[index]);
+                                      storageProduct.addFavorite(storageProduct
+                                          .listProductFavorite[index].id);
+                                    },
+                                    child: SizedBox(
+                                      width: width * .2,
+                                      child: Text(
+                                        "Excluir",
+                                        style: AppStyle.textTitleSecondary(
+                                            size: width * .025),
+                                        textAlign: TextAlign.center,
+                                      ),
                                     ),
                                   ),
                                 ],
