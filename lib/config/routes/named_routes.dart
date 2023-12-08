a3cdfe140dd4cbcd80e273db20a5b76157dd821f
Anfacibry <jogos.fabriciocintra@gmail.com>
2023-12-08 14:52:00 -0300

    :wrench: chore: Adicionando routFavorite na linha 8 e routProductDetail na linha 9

--- lib/config/routes/named_routes.dart
+++ lib/config/routes/named_routes.dart
@@ -1,7 +1,7 @@
 class NamedRoutes {
-  static const String routeHome = "/";
-  //Tela de login
   static const String routeLogin = "/login";
+  static const String routeHome = "/home";
+  //Tela de login
   static const String routeLoginRegistration = "/loginRegistration";
   static const String routeSocialNetworkRegistration =
       "/socialNetworkRegistration";
