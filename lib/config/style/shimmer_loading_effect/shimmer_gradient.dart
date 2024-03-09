dde86b1ab71b6e58170aa3f3e8c5e6d757634c82
Anfacibry <jogos.fabriciocintra@gmail.com>
2024-03-09 10:53:22 -0300

    feat: Criando pastas com arquivos com classe de animação de brilho com gradiente ao fazer login e carregar as imagens presentes no app

---
+++ lib/config/style/shimmer_loading_effect/shimmer_gradient.dart
@@ -0,0 +1,98 @@
+import 'package:app_fashion_shop/config/style/shimmer_loading_effect/paint_shimmer_gradient.dart';
+import 'package:app_fashion_shop/config/theme/app_color.dart';
+
+import 'package:flutter/material.dart';
+
+const shimmerGradient = LinearGradient(
+  colors: [
+    AppColor.primaryContainerColor,
+    AppColor.secundaryColor,
+    AppColor.primaryContainerColor,
+  ],
+  stops: [
+    0.1,
+    0.3,
+    0.4,
+  ],
+  begin: Alignment(-2.0, -0.3),
+  end: Alignment(2.0, 0.3),
+  tileMode: TileMode.clamp,
+);
+
+class ShimmerLoading extends StatefulWidget {
+  final Widget child;
+  final bool isLoading;
+  const ShimmerLoading({
+    super.key,
+    required this.isLoading,
+    required this.child,
+  });
+
+  @override
+  State<ShimmerLoading> createState() => _ShimmerLoadingState();
+}
+
+class _ShimmerLoadingState extends State<ShimmerLoading> {
+  Listenable? _shimmerChanges;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_shimmerChanges != null) {
+      _shimmerChanges!.removeListener(_onShimmerChange);
+    }
+    _shimmerChanges = PaintShimmerGradient.of(context)?.shimmerChanges;
+    if (_shimmerChanges != null) {
+      _shimmerChanges!.addListener(_onShimmerChange);
+    }
+  }
+
+  @override
+  void dispose() {
+    _shimmerChanges?.removeListener(_onShimmerChange);
+    super.dispose();
+  }
+
+  void _onShimmerChange() {
+    if (widget.isLoading) {
+      setState(() {
+        // update the shimmer painting.
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!widget.isLoading) {
+      return widget.child;
+    }
+
+    // Collect ancestor shimmer info.
+    final shimmer = PaintShimmerGradient.of(context)!;
+    if (!shimmer.isSized) {
+      // The ancestor Shimmer widget has not laid
+      // itself out yet. Return an empty box.
+      return const SizedBox();
+    }
+    final shimmerSize = shimmer.size;
+    final gradient = shimmer.gradient;
+    final offsetWithinShimmer = shimmer.getDescendantOffset(
+      descendant: context.findAncestorRenderObjectOfType() as RenderBox,
+    );
+
+    return ShaderMask(
+      blendMode: BlendMode.srcATop,
+      shaderCallback: (bounds) {
+        return gradient.createShader(
+          Rect.fromLTWH(
+            -offsetWithinShimmer.dx,
+            -offsetWithinShimmer.dy,
+            shimmerSize.width,
+            shimmerSize.height,
+          ),
+        );
+      },
+      child: widget.child,
+    );
+  }
+}
